06c8c3278847453db359bc3e01a7ed89da0d3911
Ishan R <ishanrth907@proton.me>
2025-06-30 02:31:39 +0530

    Wrote the function to get computer's choice
    Uses Math.random to generate random no. between 1 & 3
    And returns Rock, paper or scissor

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * (4 - 1) + 1);
+    if (computerChoice === 1) {
+        return "Rock";
+    }
+    else if (computerChoice === 2) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
